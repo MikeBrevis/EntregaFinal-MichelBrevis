4881f061abe9c47135285470689bf8dcee2bec57
Michel Brevis <michel.brevis@gmail.com>
2023-05-07 01:32:59 -0400

    final

--- script.js
+++ script.js
@@ -1,137 +1,9 @@
-// Función para registrar un usuario
-function registerUser(username, password) {
-  // Verificamos si el usuario ya existe en el storage
-  if (localStorage.getItem(username)) {
-    console.log("El usuario ya existe.");
-    return;
-  }
+/* 
 
-  // Creamos un objeto con los datos del usuario
-  const user = { username, password };
-
-  // Convertimos el objeto a formato JSON y lo almacenamos en el storage
-  localStorage.setItem(username, JSON.stringify(user));
-
-  console.log("Usuario registrado correctamente.");
-}
-
-// Función para iniciar sesión
-function loginUser(username, password) {
-  // Obtenemos los datos del usuario desde el storage
-  const userData = localStorage.getItem(username);
-
-  // Verificamos si el usuario existe y si la contraseña es correcta
-  if (userData) {
-    const user = JSON.parse(userData);
-
-    if (user.password === password) {
-      console.log("Inicio de sesión exitoso.");
-      return true;
-    }
-  }
-
-  console.log("Nombre de usuario o contraseña incorrectos.");
-  return false;
-}
-
-
-
-
-//---------------------------------------------------------------------------------------
-
-
-const words = [
-    'wingspan',
-'parade',
-'harbor',
-'machine',
-'raincoat',
-'tractor',
-'compass',
-'mansion',
-'pedestrian',
-'blender',
-'grocery',
-'nutshell',
-'prospect',
-'translate',
-'admiral',
-'portable',
-'flagship',
-'printer',
-'gargoyle',
-'president',
-'cornfield',
-'tablet',
-'thriller',
-'journal',
-'sunflower',
-'horizon',
-'tourist',
-'concert',
-'bracket',
-'marble',
-'octopus',
-'voyage',
-'captain',
-'tornado',
-'carnival',
-'rooster',
-'cemetery',
-'platter',
-'stairway',
-'century',
-'guitar',
-'tripod',
-'backpack',
-'palette',
-'gymnast',
-'doormat',
-'village',
-'delegate',
-'overcoat',
-'saddle',
-'majesty',
-'airplane',
-'dolphin',
-'linen',
-'silverware',
-'carousel',
-'crimson',
-'temple',
-'miracle',
-'jungle',
-'cottage',
-'charcoal',
-'avenue',
-'falcon',
-'origami',
-'timber',
-'pigeon',
-'ambassador',
-'neighbor',
-'premiere',
-'hardware',
-'nominee',
-'cushion',
-'galaxy',
-'torrent',
-'horoscope',
-'library',
-'bookshelf',
-'necktie',
-'damask',
-'waveform',
-'jewelry',
-'typewriter',
-'groove',
-'medicine'
-  ];
-
-  //elige una palabra random del array
-function randomWords(words){
-    let random = Math.floor(Math.random() * words.length);
-    return words[random]
+//elige una palabra random del array
+function randomWords(words) {
+  let random = Math.floor(Math.random() * words.length);
+  return words[random]
 };
 
 let palabraAleatoria = randomWords(words);
@@ -139,22 +11,22 @@ let time = 10;
 let score = 0;
 
 //le damos contenido al h1 
-function addToDOM(){
-let h1 = document.querySelector('#randomWord');
-return h1.textContent = palabraAleatoria;
+function addToDOM() {
+  let h1 = document.querySelector('#randomWord');
+  return h1.textContent = palabraAleatoria;
 }
 addToDOM();
 
 //creamos un evento para comparar las palabras
 let input = document.querySelector('#text');
 
-input.addEventListener('input', function(e){
+input.addEventListener('input', function (e) {
   let palabraIngresada = input.value;
-  if(palabraIngresada === palabraAleatoria){
+  if (palabraIngresada === palabraAleatoria) {
     let suma3S = document.querySelector('#timeSpan');
-    suma3S.innerHTML = `${time+=3}s`;
+    suma3S.innerHTML = `${time += 3}s`;
     input.value = '';
-    palabraAleatoria = randomWords(words);   
+    palabraAleatoria = randomWords(words);
     addToDOM();
     updateScore();
   }
@@ -162,21 +34,21 @@ input.addEventListener('input', function(e){
 
 //manipular el tiempo
 
-function actualizarTiempo(){
-  
-    document.querySelector('#timeSpan').innerHTML = `${time -= 1}s`;
-  
-    if(time === 0){
+function actualizarTiempo() {
+
+  document.querySelector('#timeSpan').innerHTML = `${time -= 1}s`;
+
+  if (time === 0) {
     clearInterval(timeInterval);
     gameOver();
   }
 }
 
-let timeInterval = setInterval(actualizarTiempo, 1000);
+let timeInterval = setInterval(actualizarTiempo, 1000);
 
 //suma 1 puto por cada palabra correcta
-function updateScore(){
-  document.querySelector('#score').innerHTML=score+=1
+function updateScore() {
+  document.querySelector('#score').innerHTML = score += 1
 }
 
 //fin del juego cuando se acaba el tiempo
@@ -205,6 +77,136 @@ function gameOver() {
   boton.textContent = 'Volvé a empezar';
 
   endGame.appendChild(boton);
+}  */
+
+function randomWords(words) {
+  let random = Math.floor(Math.random() * words.length);
+  return words[random];
 }
 
+let palabraAleatoria = randomWords(words);
+let time = 10;
+let score = 0;
 
+function addToDOM() {
+  let h1 = document.querySelector("#randomWord");
+  return (h1.textContent = palabraAleatoria);
+}
+
+function actualizarTiempo() {
+  document.querySelector("#timeSpan").innerHTML = `${(time -= 1)}s`;
+
+  if (time === 0) {
+    clearInterval(timeInterval);
+    gameOver();
+  }
+}
+
+function updateScore() {
+  document.querySelector("#score").innerHTML = (score += 1);
+}
+
+function displayTopScores() {
+  let topScoresList = document.querySelector("#topScoresList");
+  topScoresList.innerHTML = "";
+
+  let username = localStorage.getItem("username");
+  if (username) {
+    const key = username + "-scores";
+    let scores = JSON.parse(localStorage.getItem(key)) || [];
+
+    scores.slice(0, 3).forEach((score, index) => {
+      let li = document.createElement("li");
+      li.textContent = `Puntaje ${index + 1}: ${score}`;
+      topScoresList.appendChild(li);
+    });
+  }
+}
+
+function startGame() {
+  let gameContainer = document.querySelector("#game-container");
+  gameContainer.style.display = "block";
+
+  let registerContainer = document.querySelector("#register-container");
+
+  let username = localStorage.getItem("username");
+  if (!username) {
+    let usernameInput = document.querySelector("#username");
+    username = usernameInput.value.trim();
+    if (username) {
+      localStorage.setItem("username", username);
+    } else {
+      alert("Por favor, ingresa un nombre de usuario.");
+      gameContainer.style.display = "none";
+      return;
+    }
+  } else {
+    registerContainer.style.display = "none";
+  }
+
+  addToDOM();
+  timeInterval = setInterval(actualizarTiempo, 1000);
+
+  let input = document.querySelector("#text");
+  input.addEventListener("input", function (e) {
+    let palabraIngresada = input.value;
+    if (palabraIngresada === palabraAleatoria) {
+      let suma3S = document.querySelector("#timeSpan");
+      suma3S.innerHTML = `${(time += 3)}s`;
+      input.value = "";
+      palabraAleatoria = randomWords(words);
+      addToDOM();
+      updateScore();
+    }
+  });
+
+  displayTopScores();
+}
+
+function saveScore(username, score) {
+  const key = username + "-scores";
+  let scores = JSON.parse(localStorage.getItem(key)) || [];
+  scores.push(score);
+  scores.sort((a, b) => b - a);
+  scores = scores.slice(0, 3);
+  localStorage.setItem(key, JSON.stringify(scores));
+}
+
+function gameOver() {
+  let endGame = document.querySelector("#end-game-container");
+  let h2 = document.createElement("h2");
+  h2.textContent = "¡TIEMPO!";
+  let scorePara = document.createElement("p");
+  scorePara.textContent = `Tu puntaje es: ${score}`;
+  let levelPara = document.createElement("p");
+
+  endGame.appendChild(h2);
+  endGame.appendChild(scorePara);
+  endGame.appendChild(levelPara);
+
+  if (score <= 3) {
+    levelPara.textContent = "Puedes hacerlo más rápido!";
+  } else if (score <= 6) {
+    levelPara.textContent = "Increíble, ¿podrás hacerlo mejor la próxima vez?";
+  } else {
+    levelPara.textContent = "¡Flash no se compara contigo!";
+  }
+
+  let boton = document.createElement("button");
+  boton.textContent = "Volvé a empezar";
+  boton.addEventListener("click", function () {
+    endGame.innerHTML = "";
+    time = 10;
+    score = 0;
+    startGame();
+  });
+
+  endGame.appendChild(boton);
+
+  let username = localStorage.getItem("username");
+  saveScore(username, score);
+  displayTopScores();
+}
+
+let registerButton = document.querySelector("#register-button");
+registerButton.addEventListener("click", startGame);
